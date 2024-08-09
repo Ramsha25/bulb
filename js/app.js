1a95ba980d772448df2ef24b107995e39b1d390f
Ramsha25 <ramsharaheem30@gmail.com>
2024-08-09 07:15:50 -0700

    Bulb on/off

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Turn on
+
+function bulbOn(){
+    let bulb = document.querySelector(".bulboff")
+    bulb.setAttribute("src", "images/bulbon  img.png");
+}
+
+function bulbOff(){
+    let bulb = document.querySelector(".bulboff")
+    bulb.setAttribute("src", "./images/bulboff img.png");
+}
+
